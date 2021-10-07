f85e77c9e026aa9e914048045f12138efe01091e
GabyTenor <91669924+GabyTenor@users.noreply.github.com>
2021-10-07 14:23:55 +0200

    Table de 5 allant jusque 500

---
+++ Table de 5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Table_de_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resultat = 0 + ",";
+
+            table5(ref resultat);
+
+            Console.WriteLine(resultat);
+            Console.ReadLine();
+        }
+        public static void table5(ref string resultat)
+        {
+            for(int n = 0; n<=500; n+=5 )
+            {
+                resultat = resultat + "," + n;
+            }
+        }
+    }
+}
